22a65e8c1ede0678b9cdacf90c70e0b8cb7b53be
Edgar <edgarvazevedo@gmail.com>
2021-11-01 21:20:52 +0000

    updates

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -51,7 +51,78 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener("click", (btn02) => {
 
 //por as funcções para puxar as letras do jogar para o dicas. 
-
-
-
 })
+
+
+
+  function starTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+
+    setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds; 
+
+    if(--timer < 0){
+    timer = duration;
+    }
+
+    }, 1000)
+
+    }
+
+
+  window.onload = function() {
+
+    const duration = 60 * 1;
+    const display = document.querySelector("#timer");
+
+    starTimer(duration, display);
+    
+   }
+
+
+
+ 
+    const formData = new FormData(document.querySelector("#myForm"))
+    document.getElementById('myForm').addEventListener('submit', element => {
+    element.preventDefault()
+
+    const valorInput = new FormData(element.target)
+
+    const myObject = Object.fromEntries(valorInput)
+    console.log('meu objeto', myObject)
+
+    localStorage.setItem('player1', myObject.player1)
+    localStorage.setItem('player2', myObject.player2)
+    window.location.replace("../../index.html")
+
+    
+})
+
+        const firstPlayer = document.getElementById('player1')
+        firstPlayer.innerHTML = 'firstName: ' + localStorage.getItem('tela2jogador1')
+
+        const secondPlayer = document.getElementById('player2')
+        secondPlayer.innerHTML = 'secondName: ' + localStorage.getItem('tela2jogador2')
+
+
+
+
+// agora passar para a segunda tela 
+
+
+
+
+
+// document.getElementById("player1").value = "Johnny Bravo";
+
+
+// colocar meme gretchem quando chegar a 0!!!!!!!!!
+/* const myWindow = window.open("", "", "width=200, height=100");
+    myWindow.document.write("<p> olar </p>");
+    setTimeout(function(){ myWindow.close() }, 5000); */
