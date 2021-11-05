485e6df2d87ee9a7ba569f3c291cad6f2174733b
Edgar <edgarvazevedo@gmail.com>
2021-11-05 16:20:26 +0000

    final

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
  DESCRIÇÃO DO PROJETO  
 ========================
 
-<p>Projeto fetio baseado no jogo Gartic</p>
+<p>Projeto fetio baseado no jogo Gartic. 
+Pojeto foi feito durante uma semana do Bootcamp da Ironhack.</p>
 
 <p> Link do deploy: https://edgarvazevedo.github.io/jogo-desenho/ <p>
 
-<p> Link da apresentação: 
+<p> Link da apresentação: https://docs.google.com/presentation/d/1mT3URdi1LYAYHfSq3AMH1egOz8o4ofu8ag87tkEIiss/edit?usp=sharing </p>
--- assets/scripts/segunTela.js
+++ assets/scripts/segunTela.js
@@ -1,21 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
 
-    const pincel = {
+    let pincel = {
         ativo: false,
         movendo: false,
         pos: { x: 0, y: 0 },
         posAnterior: null
     }
 
-    const tela = document.querySelector('#tela');
+    const tela = document.getElementById('canvas');
     const context = tela.getContext('2d')
 
 
-    tela.width = 600;
-    tela.height = 600;
-
     context.lineWidth = 1;
-    context.strokeStyle = document.getElementById('color');
+    context.strokeStyle = document.getElementById('red');
     
     
    
@@ -23,8 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const desenharLinha = (linha) => {
 
         context.beginPath();
-        context.moveTo(linha.posAnterior.x, linha.posAnterior.y);
-        context.lineTo(linha.pos.x, linha.pos.y)
+        context.moveTo(pincel.pos.x, pincel.pos.y);
+        reposition(linha)
+        context.lineTo(pincel.pos.x, pincel.pos.y);
         context.stroke();
     }
 
@@ -33,9 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
     tela.onmouseup = (evento) => {pincel.ativo = false};
 
     tela.onmousemove = (evento) => {
-        pincel.pos.x = evento.clientX
-        pincel.pos.y = evento.clientY
         pincel.movendo = true;
+        reposition (evento)
+        
+    }
+
+    function reposition (event) {
+        pincel.pos.x = event.clientX - canvas.offsetLeft;
+        pincel.pos.y = event.clientY - canvas.offsetTop;
     }
 
     const ciclo = () => {
@@ -48,8 +51,46 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(ciclo, 10);
     }
     ciclo()
-})
+})*/
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let coord = { x: 0, y: 0 };
+
+canvas.width = 600;
+canvas.height = 600;
+
+document.addEventListener("mousedown", start);
+document.addEventListener("mouseup", stop);
+window.addEventListener("resize", resize);
+
+resize();
+
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+function reposition(event) {
+  coord.x = event.clientX - canvas.offsetLeft;
+  coord.y = event.clientY - canvas.offsetTop;
+}
+function start(event) {
+  document.addEventListener("mousemove", draw);
+  reposition(event);
+}
+function stop() {
+  document.removeEventListener("mousemove", draw);
+}
+function draw(event) {
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "black";
+  ctx.moveTo(coord.x, coord.y);
+  reposition(event);
+  ctx.lineTo(coord.x, coord.y);
+  ctx.stroke();
+}
 
 
 
@@ -80,7 +121,7 @@ function starTimer(duration, display) {
 
   window.onload = function() {
 
-    const duration = 60 * 1;
+    const duration = 60 * 0.5;
     const display = document.querySelector("#timer");
     var adivinhar = "Objeto: ";
 
